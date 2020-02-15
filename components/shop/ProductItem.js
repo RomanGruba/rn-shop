a2cecb2ab1493fccbef06aa949e67df236030a4e
Roman Gruba <roman.gruba@gmail.com>
2020-02-15 22:23:04 +0200

    productOverview

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { Text, View, Image, StyleSheet, Button } from "react-native";
+import Colors from "../../constants/Colors";
 
 const ProductItem = props => {
   return (
     <View style={styles.product}>
-      <Image source={{ uri: props.image }} />
-      <Text>{props.title}</Text>
-      <Text>${props.price.toFixed(2)}</Text>
-      <View>
-        <Button title="View Details" onPress={props.onViewDetail} />
-        <Button title="To Cart" onPress={props.onAddToCart} />
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: props.image }} style={styles.image} />
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+      </View>
+      <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title="View Details"
+          onPress={props.onViewDetail}
+        />
+        <Button
+          color={Colors.primary}
+          title="To Cart"
+          onPress={props.onAddToCart}
+        />
       </View>
     </View>
   );
@@ -26,6 +39,38 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     height: 300,
     margin: 20
+  },
+
+  imageContainer: {
+    width: "100%",
+    height: "60%",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: "hidden"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 4
+  },
+  price: {
+    fontSize: 14,
+    color: "#888"
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 20
+  },
+  details: {
+    alignItems: "center",
+    height: "15%",
+    padding: 4
   }
 });
 
