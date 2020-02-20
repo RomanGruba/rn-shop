f626255d4da25b91f912c1df3448a3cfeb59cc19
Roman Gruba <roman.gruba@gmail.com>
2020-02-20 23:42:30 +0200

    moment

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import CartItem from "./CartItem";
+import Colors from "../../constants/Colors";
 
 const OrderItem = props => {
   return (
@@ -9,11 +10,39 @@ const OrderItem = props => {
         <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
-      <Button title="Show details" />
+      <Button color={Colors.primary} title="Show details" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  orderItem: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    margin: 20,
+    padding: 10,
+    alignItems: "center"
+  },
+  summary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%"
+  },
+  totalAmount: {
+    fontFamily: "open-sans-bold",
+    fontSize: 16
+  },
+  date: {
+    fontFamily: "open-sans",
+    fontSize: 16,
+    color: "#888"
+  }
+});
 
 export default OrderItem;
