47b085403ab9249c9c211f0112c94a5f2ce0e699
Roman Gruba <roman.gruba@gmail.com>
2020-03-01 22:30:05 +0200

    auth validation

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useReducer, useCallback } from "react";
 import {
   ScrollView,
   StyleSheet,
@@ -6,12 +6,75 @@ import {
   KeyboardAvoidingView,
   Button
 } from "react-native";
+import { useDispatch } from "react-redux";
 import Input from "../../components/UI/Input";
 import Card from "../../components/UI/Card";
 import Colors from "../../constants/Colors";
 import { LinearGradient } from "expo-linear-gradient";
+import * as authActions from "../../store/actions/authActions";
+
+const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE";
+
+const formReducer = (state, action) => {
+  if ((action.type = FORM_INPUT_UPDATE)) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.input]: action.value
+    };
+    const updatedValidities = {
+      ...state.inputValidities,
+      [action.input]: action.isValid
+    };
+    let updatedFormIsValid = true;
+    for (let key in updatedValidities) {
+      updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+    }
+    return {
+      formIsValid: updatedFormIsValid,
+      inputValidities: updatedValidities,
+      inputValues: updatedValues
+    };
+  }
+  return state;
+};
 
 const AuthScreen = props => {
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: "",
+      password: ""
+    },
+    inputValidities: {
+      email: false,
+      password: false
+    },
+    formIsValid: false
+  });
+
+  const inputChangeHandler = useCallback(
+    (inputIdetifier, inputValue, inputValidity) => {
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE,
+        value: inputValue,
+        isValid: inputValidity,
+        input: inputIdetifier
+      });
+    },
+    [dispatchFormState]
+  );
+
+  const signupHandler = () => {
+    dispatch(
+      authActions.signup(
+        formState.inputValues.email,
+        formState.inputValues.password
+      )
+    );
+  };
+
   return (
     <KeyboardAvoidingView
       behavior="padding"
@@ -28,29 +91,32 @@ const AuthScreen = props => {
               required
               email
               autoCapitalize="none"
-              errorMessage="Please enter a valid email address."
-              onInputChange={() => {}}
+              errorText="Please enter a valid email address."
+              onInputChange={inputChangeHandler}
               initialValue=""
             />
             <Input
               id="password"
               label="Password"
               keyboardType="default"
-              secureTextEntry
+              //secureTextEntry
               minLength={5}
               required
-              email
               autoCapitalize="none"
-              errorMessage="Please enter a valid password."
-              onInputChange={() => {}}
+              errorText="Please enter a valid password."
+              onInputChange={inputChangeHandler}
               initialValue=""
             />
             <View style={styles.buttonContainer}>
-              <Button title="Login" color={Colors.primary} onPress={() => {}} />
+              <Button
+                title={isSignup ? "Sign Up" : "Login"}
+                color={Colors.primary}
+                onPress={signupHandler}
+              />
             </View>
             <View style={styles.buttonContainer}>
               <Button
-                title="Switch to sign up"
+                title={`Switch to ${isSignup ? "Login" : "Sign Up"}`}
                 color={Colors.accent}
                 onPress={() => {}}
               />
---
+++ store/actions/authActions.js
@@ -0,0 +1,54 @@
+export const SIGNUP = "SIGNUP";
+export const LOGIN = "LOGIN";
+
+export const signup = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDL4iHmDxz7QT_UTI-jYiv37ELc2Gr7l9k",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Somthing went wrong...");
+    }
+
+    const resData = await response.json();
+    dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDL4iHmDxz7QT_UTI-jYiv37ELc2Gr7l9k",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Somthing went wrong...");
+    }
+
+    const resData = await response.json();
+    dispatch({ type: LOGIN });
+  };
+};
